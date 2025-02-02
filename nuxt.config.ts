9db803ff13abddd8b7de4b3758e223900bd22c2c
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-02-02 11:44:23 +0300

    fix: enable component islands

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
+    experimental: {
+        componentIslands: true,
+    },
     devtools: { enabled: true },
     modules: [
         "@nuxt/fonts",
