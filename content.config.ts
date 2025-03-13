3018e41ccb6923c8d83f09705702724d7ddfedf1
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-03-13 14:41:08 +0300

    fix: sitemap & content integration

--- content.config.ts
+++ content.config.ts
@@ -1,16 +1,18 @@
 import { defineContentConfig, defineCollection, z } from "@nuxt/content";
+import { asSitemapCollection } from "@nuxtjs/sitemap/content";
 
 export default defineContentConfig({
     collections: {
-        content: defineCollection({
-            type: "page",
-            source: "**/*.md",
-            schema: z.object({
-                index: z.number(),
-                date: z.string(),
-                image: z.string(),
-            }),
-        }),
+        content: defineCollection(
+            asSitemapCollection({
+                type: "page",
+                source: "**/*.md",
+                schema: z.object({
+                    index: z.number(),
+                    date: z.string(),
+                    image: z.string(),
+                }),
+            })
+        ),
     },
 });
-	
