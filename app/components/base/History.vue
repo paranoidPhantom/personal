1ea6c8408a62dfc3d7fd2b1f32ea4fa1325df04c
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-04-07 14:43:49 +0300

    feat: optimize home rendering via article cutoff

--- app/components/base/History.vue
+++ app/components/base/History.vue
@@ -17,8 +17,9 @@ const { data: contentList } = await useAsyncData(
         watch: [locale],
     }
 );
-
 const router = useRouter();
+
+const cutoffIndex = 5;
 </script>
 
 <template>
@@ -29,83 +30,62 @@ const router = useRouter();
         }"
     >
         <hr />
-        <ClientOnly>
-            <Card
-                v-for="(item, index) in contentList ?? []"
-                :key="`${locale}-${index}`"
-                data-aos="fade-up"
-                :data-aos-delay="index * 100"
-            >
-                <CardHeader>
-                    <NuxtImg
-                        class="absolute top-0 left-0 w-full h-auto object-cover rounded-t-lg"
-                        v-show="item.image"
-                        :src="item.image"
-                        alt="Cover graphic"
-                        width="1000px"
-                        style="
-                            mask-image: linear-gradient(
-                                to bottom,
-                                black 0%,
-                                black 95%,
-                                transparent 100%
-                            );
-                        "
-                    />
-                    <NuxtImg
-                        class="opacity-0"
-                        v-show="item.image"
-                        :src="item.image"
-                        alt="Cover graphic"
-                    />
-                    <CardTitle>
-                        <RadiantText class="inline-flex max-w-full py-2">
-                            <span>
-                                {{ item.title }}
-                            </span>
-                        </RadiantText>
-                    </CardTitle>
-                    <CardDescription class="opacity-50">{{
-                        item.description
-                    }}</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <MDFormatter class="h-96 overflow-hidden relative">
-                        <ContentRenderer :value="item.body" />
-                        <div
-                            class="bg-gradient-to-t from-background h-64 w-full absolute bottom-0 z-10 flex items-end from-20%"
-                        >
-                            <InteractiveHoverButton
-                                :text="$t('read_more')"
-                                @click="navigateTo(`/read${item.path}`)"
-                            />
-                        </div>
-                    </MDFormatter>
-                </CardContent>
-                <CardFooter class="opacity-20">{{ item.date }}</CardFooter>
-            </Card>
-            <template #fallback>
-                <Card v-for="i in (contentList ?? []).length" :key="i">
-                    <CardHeader>
-                        <CardTitle>
-                            <Skeleton class="w-[200px] h-8 rounded-full" />
-                        </CardTitle>
-                        <CardDescription>
-                            <Skeleton class="w-[150px] h-6 rounded-full" />
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <div class="flex flex-col gap-2">
-                            <Skeleton class="w-full h-4 rounded-full" />
-                            <Skeleton class="w-full h-4 rounded-full" />
-                            <Skeleton class="w-full h-4 rounded-full" />
-                        </div>
-                    </CardContent>
-                    <CardFooter class="opacity-20">
-                        <Skeleton class="w-[100px] h-4 rounded-full" />
-                    </CardFooter>
-                </Card>
-            </template>
-        </ClientOnly>
+        <Card
+            v-for="(item, index) in contentList?.map((post) => {
+                post.body.value.splice(cutoffIndex, post.body.value.length);
+                return post;
+            }) ?? []"
+            :key="`${locale}-${index}`"
+            data-aos="fade-up"
+            :data-aos-delay="index * 100"
+        >
+            <CardHeader>
+                <NuxtImg
+                    class="absolute top-0 left-0 w-full h-auto object-cover rounded-t-lg"
+                    v-show="item.image"
+                    :src="item.image"
+                    alt="Cover graphic"
+                    width="1000px"
+                    style="
+                        mask-image: linear-gradient(
+                            to bottom,
+                            black 0%,
+                            black 95%,
+                            transparent 100%
+                        );
+                    "
+                />
+                <NuxtImg
+                    class="opacity-0"
+                    v-show="item.image"
+                    :src="item.image"
+                    alt="Cover graphic"
+                />
+                <CardTitle>
+                    <RadiantText class="inline-flex max-w-full py-2">
+                        <span>
+                            {{ item.title }}
+                        </span>
+                    </RadiantText>
+                </CardTitle>
+                <CardDescription class="opacity-50">{{
+                    item.description
+                }}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <MDFormatter class="h-auto overflow-hidden relative">
+                    <ContentRenderer :value="item.body" />
+                    <div
+                        class="bg-gradient-to-t from-background h-64 w-full absolute bottom-0 z-10 flex items-end from-20%"
+                    >
+                        <BaseHistoryButton
+                            :to="`/read${item.path}`"
+                            :key="item.path"
+                        />
+                    </div>
+                </MDFormatter>
+            </CardContent>
+            <CardFooter class="opacity-20">{{ item.date }}</CardFooter>
+        </Card>
     </div>
 </template>
