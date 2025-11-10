3f58c9bc8b951ad1cfdfd234613c22dcf7788f51
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-11-10 12:47:27 +0300

    feat: set up studio and content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,24 @@ export default defineNuxtConfig({
         "@nuxt/fonts",
         "@nuxtjs/tailwindcss",
         "@nuxtjs/color-mode",
+        "@vueuse/nuxt",
+        "nuxt-studio",
     ],
     css: ["@/assets/css/index.scss"],
     colorMode: {
         preference: "dark",
         fallback: "dark",
         classSuffix: "",
+    },
+    studio: {
+        route: "/_studio",
+
+        // GitHub repository configuration (owner and repo are required)
+        repository: {
+            provider: "github",
+            owner: "paranoidPhantom",
+            repo: "personal",
+            branch: "master",
+        },
     },
 });
