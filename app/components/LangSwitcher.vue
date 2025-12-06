df6f11340754875c1e8029a43c8b9a4fdec592b9
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-12-06 19:52:21 +0300

    refactor: lang switcher

--- app/components/LangSwitcher.vue
+++ app/components/LangSwitcher.vue
@@ -3,9 +3,7 @@ const { locale } = useI18n();
 </script>
 
 <template>
-    <div
-        class="fixed top-0 right-0 flex gap-4 backdrop-blur-lg p-4 z-50 rounded-bl-xl"
-    >
+    <div class="fixed top-4 right-4 flex gap-4 text-sm">
         <NuxtLink v-if="locale === 'ru'" :to="$switchLocalePath('en')">
             English
         </NuxtLink>
