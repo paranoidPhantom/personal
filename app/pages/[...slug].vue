422594d8db34178e4ec5cefb70be7849bca124ac
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-12-06 19:52:37 +0300

    feat: add middleware

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,5 +1,22 @@
 <script lang="ts" setup>
-const { locale } = useI18n();
+import type { RouteLocationNormalizedGeneric } from "vue-router";
+
+definePageMeta({
+    middleware: [
+        async (
+            to: RouteLocationNormalizedGeneric,
+            from: RouteLocationNormalizedGeneric
+        ) => {
+            const page = await queryCollection("content").path(to.path).first();
+            if (!page) {
+                return abortNavigation({
+                    statusCode: 404,
+                    statusMessage: "Page not found",
+                });
+            }	
+        },
+    ],
+});
 
 const { path } = useRoute();
 const contentPath = computed(() => {
@@ -22,15 +39,21 @@ const { data } = useAsyncData(
 </script>
 
 <template>
-    <div v-if="data" class="mx-auto flex max-w-[1000px] w-full flex-col p-8">
-        <NuxtLink :to="$localePath('/')" class="flex items-center">
+    <div
+        v-if="data"
+        class="mx-auto flex max-w-[1000px] w-full flex-col p-8 space-y-6"
+    >
+        <NuxtLink
+            :to="$localePath('/')"
+            class="flex items-center absolute top-4 left-4 space-x-2 text-sm opacity-70 hover:opacity-100 transition-opacity"
+        >
             <Icon name="mdi:arrow-left" class="text-lg" />
             Назад
         </NuxtLink>
         <MarkdownFormatter>
             <ContentRenderer :value="data" />
         </MarkdownFormatter>
-        <p class="mt-4 opacity-50 mr-auto">
+        <p class="opacity-50 mr-auto">
             <i>{{ data.date }}</i>
         </p>
     </div>
