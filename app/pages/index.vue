aa7c35382459dd18c0f58391bde405636993f241
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-12-12 14:18:00 +0300

    feat: add links

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,13 +9,30 @@ useHead({
         <HeroSection />
         <Technologies />
         <Articles />
-        <a
-            class="mb-16 flex items-center gap-2"
-            href="/static/resume.pdf"
-            download="hudalla-dev-resume.pdf"
-        >
-            <Icon name="mdi:document" />
-            {{ $t("download_resume") }}
-        </a>
+        <div class="flex justify-center gap-4 items-center mb-16">
+            <a
+                class="flex items-center gap-2"
+                href="/static/resume.pdf"
+                download="hudalla-dev-resume.pdf"
+            >
+                <Icon name="mdi:document" />
+                {{ $t("download_resume") }}
+            </a>
+            <div class="border-r border-r-neutral-700 shrink-0 h-8" />
+            <a
+                href="https://github.com/paranoidPhantom"
+                target="_blank"
+                class="leading-4"
+            >
+                <Icon class="size-5" name="mdi:github" />
+            </a>
+            <a
+                href="https://t.me/paranoidPhantom"
+                target="_blank"
+                class="leading-4"
+            >
+                <Icon class="size-5" name="mdi:telegram" />
+            </a>
+        </div>
     </div>
 </template>
