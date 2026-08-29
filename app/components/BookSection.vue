26629c1df5b33c58d687036c90a6e94e8c3d5289
Andrei Hudalla <andrei@hudalla.dev>
2026-08-29 18:57:15 +0300

    books(feat): use profile link

--- app/components/BookSection.vue
+++ app/components/BookSection.vue
@@ -22,7 +22,7 @@ const { data } = useFetch("/api/books")
           <p class="opacity-80">{{ data.description }}
           </p>
           <div class="flex justify-end">
-              <NuxtLink class="mt-4 flex items-center gap-2" target="_blank" :to="`https://literal.club/book/${data.slug}`">
+              <NuxtLink class="mt-4 flex items-center gap-2" target="_blank" to="https://literal.club/ahudalla">
                   {{ $t("fetched_from_literal") }}
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><!-- Icon from Simple Icons by Simple Icons Collaborators - https://github.com/simple-icons/simple-icons/blob/develop/LICENSE.md --><path fill="currentColor" d="m11.148 5.719l.128-1.798l3.234.223l-1.52-2.902l1.63-.836L16.176 3.4L18.142.65l1.495 1.05l-1.916 2.665l3.212.221l-.128 1.797l-3.167-.217l1.498 2.878l-1.628.836l-1.578-3.017l-1.99 2.771l-1.495-1.05L14.36 5.94zm-8.129 9.513L5.197 0l2.569.355l-1.817 12.708l5.978.825l-.361 2.525zM20.981 21.7L4.328 24l-.36-2.524l16.652-2.3z"/></svg>
               </NuxtLink>
