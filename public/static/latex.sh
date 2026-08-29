a2caafbdbda05d85344bd1dc76d71fe05a4f71fe
Andrei Hudalla <andrei@hudalla.dev>
2026-08-29 15:34:53 +0300

    script(fix): rely on nix

--- public/static/latex.sh
+++ public/static/latex.sh
@@ -1,6 +1,10 @@
+cd public/static
+
 curl -fsSL "https://raw.githubusercontent.com/DPDmancul/Apple-Emoji/refs/heads/master/apple_emoji.sty" -o "apple_emoji.sty"
 mkdir -p "emoji"
 curl -fsSL "https://raw.githubusercontent.com/DPDmancul/Apple-Emoji/refs/heads/master/emoji/2B50.png" -o "emoji/2B50.png"
-texliveonfly resume.tex
-texliveonfly resume-en.tex
+
+echo "pdflatex resume.tex && pdflatex resume-en.tex" \
+  | nix shell nixpkgs#texliveFull
+
 rm -f "resume".bak* "resume-en".bak*
